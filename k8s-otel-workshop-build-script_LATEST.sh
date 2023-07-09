7dd2b654157d8f8858e603d56d22274dd0ae272e
Gerry D'Costa <32919191+gdcosta@users.noreply.github.com>
2023-07-08 22:03:11 -0700

    Add files via upload

---
+++ k8s-otel-workshop-build-script_LATEST.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#
+# Kubernetes Workshop Build Script - Linux Ubuntu
+#
+#
+# author:  Gerry D'Costa
+# title:   Staff Solutions Engineer, Splunk
+#
+# version: 1.0.0
+
+
+date_string="$(date)"
+echo "** $date_string - buildScript - start - build script" >> ~/k8s-otel-workshop.log; sleep 5
+if [ $COMPLETE_WORKSHOP_BUILD = "yes" ]; then
+	~/k8s-otel-workshop/k8s-otel-workshop-01-FW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+	~/k8s-otel-workshop/k8s-otel-workshop-02-FW2-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+	~/k8s-otel-workshop/k8s-otel-workshop-03-AW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+	~/k8s-otel-workshop/k8s-otel-workshop-04-AW2-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+	date_string="$(date)"
+	echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
+	exit 0
+else
+	if [ $SETUP_FOR_ADVANCED_2_WORKSHOP = "yes" ]; then
+		~/k8s-otel-workshop/k8s-otel-workshop-01-FW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+		~/k8s-otel-workshop/k8s-otel-workshop-02-FW2-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+		~/k8s-otel-workshop/k8s-otel-workshop-03-AW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+		date_string="$(date)"
+		echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
+		exit 0
+	else
+		if [ $SETUP_FOR_ADVANCED_1_WORKSHOP = "yes" ]; then
+			~/k8s-otel-workshop/k8s-otel-workshop-01-FW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+			~/k8s-otel-workshop/k8s-otel-workshop-02-FW2-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+			date_string="$(date)"
+			echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
+			exit 0
+		else
+			if [ $SETUP_FOR_FOUNDATIONAL_2_WORKSHOP = "yes" ]; then
+				~/k8s-otel-workshop/k8s-otel-workshop-01-FW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+				date_string="$(date)"
+				echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
+				exit 0
+			else
+				date_string="$(date)"
+				echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
+				exit 0
+			fi
+		fi
+	fi
+fi
