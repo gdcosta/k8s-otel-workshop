c4eab9206061a7624aba1721add36db3861b5c2f
Gerry D'Costa <32919191+gdcosta@users.noreply.github.com>
2024-03-01 10:53:58 -0800

    Update k8s-otel-workshop-build-script_LATEST.sh
    Updated for new workshop format

--- k8s-otel-workshop-build-script_LATEST.sh
+++ k8s-otel-workshop-build-script_LATEST.sh
@@ -12,7 +12,7 @@
 
 date_string="$(date)"
 echo "** $date_string - buildScript - start - build script" >> ~/k8s-otel-workshop.log; sleep 5
-if [ $COMPLETE_WORKSHOP_BUILD = "yes" ]; then
+if [ $COMPLETE_WORKSHOP_BUILD_ALL = "yes" ]; then
 	~/k8s-otel-workshop/k8s-otel-workshop-01-FW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
 	~/k8s-otel-workshop/k8s-otel-workshop-02-FW2-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
 	~/k8s-otel-workshop/k8s-otel-workshop-03-AW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
@@ -21,30 +21,38 @@ if [ $COMPLETE_WORKSHOP_BUILD = "yes" ]; then
 	echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
 	exit 0
 else
-	if [ $SETUP_FOR_ADVANCED_2_WORKSHOP = "yes" ]; then
+	if [ $COMPLETE_WORKSHOP_BUILD_ADVANCED_2 = "yes" ]; then
 		~/k8s-otel-workshop/k8s-otel-workshop-01-FW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
 		~/k8s-otel-workshop/k8s-otel-workshop-02-FW2-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
-		~/k8s-otel-workshop/k8s-otel-workshop-03-AW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+		~/k8s-otel-workshop/k8s-otel-workshop-04-AW2-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
 		date_string="$(date)"
 		echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
 		exit 0
-	else
-		if [ $SETUP_FOR_ADVANCED_1_WORKSHOP = "yes" ]; then
+		if [ $COMPLETE_WORKSHOP_BUILD_ADVANCED_1 = "yes" ]; then
 			~/k8s-otel-workshop/k8s-otel-workshop-01-FW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
 			~/k8s-otel-workshop/k8s-otel-workshop-02-FW2-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+			~/k8s-otel-workshop/k8s-otel-workshop-03-AW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
 			date_string="$(date)"
 			echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
 			exit 0
 		else
-			if [ $SETUP_FOR_FOUNDATIONAL_2_WORKSHOP = "yes" ]; then
+			if [ $SETUP_FOR_ADVANCED_1_OR_2_WORKSHOP = "yes" ]; then
 				~/k8s-otel-workshop/k8s-otel-workshop-01-FW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+				~/k8s-otel-workshop/k8s-otel-workshop-02-FW2-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
 				date_string="$(date)"
 				echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
 				exit 0
 			else
-				date_string="$(date)"
-				echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
-				exit 0
+				if [ $SETUP_FOR_FOUNDATIONAL_2_WORKSHOP = "yes" ]; then
+					~/k8s-otel-workshop/k8s-otel-workshop-01-FW1-tasks_LATEST.sh >> ~/k8s-otel-workshop.log; sleep 5
+					date_string="$(date)"
+					echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
+					exit 0
+				else
+					date_string="$(date)"
+					echo "** $date_string - buildScript - stop - build script" >> ~/k8s-otel-workshop.log; sleep 5
+					exit 0
+				fi
 			fi
 		fi
 	fi
